189052453051c5028268a04f37470af07680a17c
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-05-23 18:40:40 +0900

    add constructor

--- moveit_extensions/include/moveit_extensions/moveit_extensions.h
+++ moveit_extensions/include/moveit_extensions/moveit_extensions.h
@@ -6,8 +6,11 @@
 namespace moveit_extensions {
   class InterpolatedPropagationDistanceField : public distance_field::PropagationDistanceField {
   public:
+    using PropagationDistanceField::PropagationDistanceField; // 継承コンストラクタ
+
     // cellはresolution間隔で並んでいる. デフォルトではxyzに対応するcellの値をそのまま返すので、cellの境界で値が不連続で変化し、また、cellの中では値が変わらない. これは、勾配を利用した干渉回避には不適切である.
     // cellの値はcellの中心の値であるとし、周囲8cellの中心の値を線形補間して返す.
+    // getDistance()関数はPropagationDistanceFieldクラス中の各所で使われており、高速性が重要であるため、overrideしない.
     double getInterpolatedDistance(double x, double y, double z) const;
   };
 };
