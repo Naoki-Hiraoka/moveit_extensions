c41290aedd7e3567c28bbb094d10b5ecd80aa676
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2024-05-23 19:38:52 +0900

    fix

--- moveit_extensions/include/moveit_extensions/moveit_extensions.h
+++ moveit_extensions/include/moveit_extensions/moveit_extensions.h
@@ -10,8 +10,16 @@ namespace moveit_extensions {
 
     // cellはresolution間隔で並んでいる. デフォルトではxyzに対応するcellの値をそのまま返すので、cellの境界で値が不連続で変化し、また、cellの中では値が変わらない. これは、勾配を利用した干渉回避には不適切である.
     // cellの値はcellの中心の値であるとし、周囲8cellの中心の値を線形補間して返す.
-    // getDistance()関数はPropagationDistanceFieldクラス中の各所で使われており、高速性が重要であるため、overrideしない.
-    double getInterpolatedDistance(double x, double y, double z) const;
+    // getDistance(int x, int y, int z)関数はPropagationDistanceFieldクラス中の各所で使われており、高速性が重要であるため、overrideしない. 一方、getDistance(double x, double y, double z)は使われていないので、overrideする.
+    double getDistance(double x, double y, double z) const override;
+
+    // 一応定義しないと、getDistance(1,1,1)がgetDistance(double x, double y, double z)に推論されてしまう.
+    double getDistance(int x, int y, int z) const override;
+
+    // DistanceField::getDistanceGradientはvirtual定義されていないことに注意
+    // DistanceField::getDistanceGradientは内部でgetDistance(int x, int y, int z)を使ってdistanceを返す. こちらはgetDistance(double x, double y, double z)を使う
+    double getDistanceGradient(double x, double y, double z, double& gradient_x, double& gradient_y, double& gradient_z,
+                               bool& in_bounds) const;
   };
 };
 
--- moveit_extensions/src/moveit_extensions.cpp
+++ moveit_extensions/src/moveit_extensions.cpp
@@ -1,16 +1,16 @@
 #include <moveit_extensions/moveit_extensions.h>
 
 namespace moveit_extensions {
-  double InterpolatedPropagationDistanceField::getInterpolatedDistance(double x, double y, double z) const {
+  double InterpolatedPropagationDistanceField::getDistance(double x, double y, double z) const {
     double rx = (x - origin_x_) / resolution_;
     double ry = (y - origin_y_) / resolution_;
     double rz = (z - origin_z_) / resolution_;
-    // 小さい側のcell
-    int ix = rx;
-    int iy = ry;
-    int iz = rz;
+    // 小さい側のcell. worldToGridを使うと近いcellになってしまう
+    int ix = std::floor(rx);
+    int iy = std::floor(ry);
+    int iz = std::floor(rz);
     if (ix < 0 || iy < 0 || iz < 0 || ix >= getXNumCells() - 1 || iy >= getYNumCells() - 1 || iz >= getZNumCells() - 1){
-      return getDistance(x,y,z);
+      return PropagationDistanceField::getDistance(x,y,z);
     }
     // 小さい側のcellからの比率
     rx -= ix;
@@ -25,4 +25,36 @@ namespace moveit_extensions {
       + getDistance(ix, iy + 1, iz + 1) * (1.0 - rx) * ry * rz
       + getDistance(ix + 1, iy + 1, iz + 1) * rx * ry * rz;
   }
+
+  double InterpolatedPropagationDistanceField::getDistance(int x, int y, int z) const {
+    return PropagationDistanceField::getDistance(x,y,z);
+  }
+
+  double InterpolatedPropagationDistanceField::getDistanceGradient(double x, double y, double z, double& gradient_x, double& gradient_y,
+                                                                   double& gradient_z, bool& in_bounds) const
+  {
+    int gx, gy, gz;
+
+    worldToGrid(x, y, z, gx, gy, gz);
+
+    // if out of bounds, return max_distance, and 0 gradient
+    // we need extra padding of 1 to get gradients
+    if (gx < 1 || gy < 1 || gz < 1 || gx >= getXNumCells() - 1 || gy >= getYNumCells() - 1 || gz >= getZNumCells() - 1)
+      {
+        gradient_x = 0.0;
+        gradient_y = 0.0;
+        gradient_z = 0.0;
+        in_bounds = false;
+        return getUninitializedDistance();
+      }
+
+    gradient_x = (getDistance(gx + 1, gy, gz) - getDistance(gx - 1, gy, gz)) * inv_twice_resolution_;
+    gradient_y = (getDistance(gx, gy + 1, gz) - getDistance(gx, gy - 1, gz)) * inv_twice_resolution_;
+    gradient_z = (getDistance(gx, gy, gz + 1) - getDistance(gx, gy, gz - 1)) * inv_twice_resolution_;
+
+    in_bounds = true;
+    return getDistance(x, y, z); // ここだけDistanceField::getDistanceGradientと異なる
+  }
+
+
 };
